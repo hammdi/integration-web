81e71b5ba426d1d963cd663fddb65f1f0abb38bf
Napsta <mehdi.benattaya@esprit.tn>
2025-02-28 18:47:18 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        'pharma-bg': "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/1939309.png-5J5VQ1RfPcMSLZUW3phS1k3NwuLPls.webp')",
+      },
       colors: {
-        primary: "#61A9B9", // Footer color
-        background: "#F8F8F8", // Background color matching the image
-        accent: "#AFE3F1", // Button color
+        primary: "#61A9B9",
+        background: "#F8F8F8",
+        accent: "#AFE3F1",
+        sidebar: "#AFE3F1",
       },
       spacing: {
         '128': '32rem',
@@ -15,12 +20,9 @@ module.exports = {
         'xl': '12px',
       },
       boxShadow: {
-        'custom': '0 4px 10px rgba(0, 0, 0, 0.1)',
+        'custom': '0 4px 10px rgba(0, 0, 0, 0.05)',
       },
     },
   },
   plugins: [],
 }
-
-
-
