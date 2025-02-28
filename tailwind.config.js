3d0b950d8c40e0d84b9e9ea86979a215a6f30601
hammdi <hamdi.kbaier@esprit.tn>
2025-02-28 18:42:23 +0100

    mehdi works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html", "*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'pharma-bg': "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/1939309.png-5J5VQ1RfPcMSLZUW3phS1k3NwuLPls.webp')",
-      },
       colors: {
-        primary: "#61A9B9",
-        background: "#F8F8F8",
-        accent: "#AFE3F1",
-        sidebar: "#AFE3F1",
+        primary: "#61A9B9", // Footer color
+        background: "#F8F8F8", // Background color matching the image
+        accent: "#AFE3F1", // Button color
       },
       spacing: {
         '128': '32rem',
@@ -20,9 +15,12 @@ module.exports = {
         'xl': '12px',
       },
       boxShadow: {
-        'custom': '0 4px 10px rgba(0, 0, 0, 0.05)',
+        'custom': '0 4px 10px rgba(0, 0, 0, 0.1)',
       },
     },
   },
   plugins: [],
 }
+
+
+
